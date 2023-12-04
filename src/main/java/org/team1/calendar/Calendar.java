31f075f9a956fda10244d1ca774d1f6945f56007
Dong Hyun Roh <dhr014@bucknell.edu>
2023-12-04 12:51:49 -0500

    updating 28

--- src/main/java/org/team1/calendar/Calendar.java
+++ src/main/java/org/team1/calendar/Calendar.java
@@ -58,8 +58,12 @@ public class Calendar implements Initializable {
 
     private Map<LocalDate, ArrayList<Workout>> workoutsMap;
 
+
     /**
-     * Initializes the calendar view with the current date and associated workouts.
+     * Initializes the controller with the current date and associated workouts.
+     *
+     * @param url The location used to resolve the root object, or null if not known
+     * @param resourceBundle  The resources used to localize the root object, or null if not known
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -70,6 +74,11 @@ public class Calendar implements Initializable {
         drawCalendar();
     }
 
+
+    /**
+     * Navigates to the previous month in the calendar view.
+     * Clears the calendar view and redraws it to display the previous month.
+     */
     @FXML
     void backOneMonth() {
         dateFocus = dateFocus.minusMonths(1);
@@ -77,6 +86,11 @@ public class Calendar implements Initializable {
         drawCalendar();
     }
 
+
+    /**
+     * Navigates to the next month in the calendar view.
+     * Clears the calendar view and redraws it to display the next month.
+     */
     @FXML
     void forwardOneMonth() {
         dateFocus = dateFocus.plusMonths(1);
@@ -84,32 +98,46 @@ public class Calendar implements Initializable {
         drawCalendar();
     }
 
+
     /**
      * Draws the calendar view based on the current date and associated workouts.
      * The calendar displays dates and their corresponding workouts, if any.
+     *
+     * I have used the following source
+     * @see <a href = "https://gist.github.com/Da9el00/f4340927b8ba6941eb7562a3306e93b6">
+     *   https://gist.github.com/Da9el00/f4340927b8ba6941eb7562a3306e93b6</a>
      */
     private void drawCalendar() {
+
+        // Sets the year and month labels in the UI
         year.setText(String.valueOf(dateFocus.getYear()));
         month.setText(String.valueOf(dateFocus.getMonth()));
 
+        // Retrieve dimensions and spacing information for calendar layout
         double calendarWidth = calendar.getPrefWidth();
         double calendarHeight = calendar.getPrefHeight();
         double strokeWidth = 1;
         double spacingH = calendar.getHgap();
         double spacingV = calendar.getVgap();
 
-
+        // Determine the maximum number of days in the focused month
         int monthMaxDate = dateFocus.getMonth().maxLength();
-        //Check for leap year
+
+        // Adjust February's days for leap year
         if (dateFocus.getYear() % 4 != 0 && monthMaxDate == 29) {
             monthMaxDate = 28;
         }
-        int dateOffset = ZonedDateTime.of(dateFocus.getYear(), dateFocus.getMonthValue(), 1, 0, 0, 0, 0, dateFocus.getZone()).getDayOfWeek().getValue();
 
+        // Calculate the offset of the first day of the month
+        int dateOffset = ZonedDateTime.of(dateFocus.getYear(), dateFocus.getMonthValue(),
+                1, 0, 0, 0, 0, dateFocus.getZone()).getDayOfWeek().getValue();
+
+        // Loop through the calendar grid
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
                 StackPane stackPane = new StackPane();
 
+                // Create a rectangle to represent each day in the calendar
                 Rectangle rectangle = new Rectangle();
                 rectangle.setFill(Color.TRANSPARENT);
                 rectangle.setStroke(Color.BLACK);
@@ -121,20 +149,26 @@ public class Calendar implements Initializable {
                 stackPane.getChildren().add(rectangle);
 
                 int calculatedDate = (j + 1) + (7 * i);
+
                 if (calculatedDate > dateOffset) {
                     int currentDate = calculatedDate - dateOffset;
+
                     if (currentDate <= monthMaxDate) {
+
+                        // Display the date number within the rectangle
                         Text date = new Text(String.valueOf(currentDate));
                         double textTranslationY = -(rectangleHeight / 2) * 0.75;
                         date.setTranslateY(textTranslationY);
                         stackPane.getChildren().add(date);
 
+                        // Retrieve workouts for the current date from workoutsMap and display their representations
                         LocalDate calendarDate = LocalDate.of(dateFocus.getYear(), dateFocus.getMonth(), currentDate);
                         List<Workout> workouts = workoutsMap.get(calendarDate);
                         if (workouts != null) {
                             createWorkoutsUI(workouts, rectangleHeight, rectangleWidth, stackPane);
                         }
                     }
+                    // Highlight today's date if it matches the focused year and month
                     if (today.getYear() == dateFocus.getYear() && today.getMonth() == dateFocus.getMonth() && today.getDayOfMonth() == currentDate) {
                         rectangle.setStroke(Color.BLUE);
                     }
@@ -142,7 +176,6 @@ public class Calendar implements Initializable {
                 calendar.getChildren().add(stackPane);
             }
         }
-
     }
 
 
@@ -155,8 +188,10 @@ public class Calendar implements Initializable {
      * @param stackPane       StackPane for placing the workout UI elements
      */
     private void createWorkoutsUI(List<Workout> workouts, double rectangleHeight, double rectangleWidth, StackPane stackPane) {
+
         VBox workoutsBox = new VBox();
 
+        // Iterate through each Workout object and create a Text element for each workout
         for (Workout workout : workouts) {
             Text text = new Text(workout.getWorkoutType().toString());
             text.setUserData(workout); // Store Workouts object in the userData property
@@ -165,6 +200,7 @@ public class Calendar implements Initializable {
             text.setOnMouseClicked(this::handleWorkoutTextClick); // Set mouse click event handler
         }
 
+        // Set layout and styling properties for the workoutsBox
         workoutsBox.setTranslateY((rectangleHeight / 2) * 0.20);
         workoutsBox.setMaxWidth(rectangleWidth * 0.8);
         workoutsBox.setMaxHeight(rectangleHeight * 0.65);
@@ -173,21 +209,31 @@ public class Calendar implements Initializable {
     }
 
     /**
-     * Handles the mouse click event on workout text elements to display the workouts for the clicked date.
+     * Handles the mouse click event on a Text element representing a Workout.
+     * Shows workout information when a Workout Text is clicked.
      *
-     * @param event Mouse click event triggering the display of workouts
+     * @author Dong Hyun Roh
+     * @param event The MouseEvent generated from the click event
      */
     private void handleWorkoutTextClick(MouseEvent event) {
+
+        // Check if the event source is a Text element
         if (event.getSource() instanceof Text) {
             Text text = (Text) event.getSource();
             Object userData = text.getUserData();
 
+            // Check if the user data associated with the Text is an instance of Workout
             if (userData instanceof Workout) {
                 Workout clickedWorkout = (Workout) userData;
                 LocalDate workoutDate = clickedWorkout.getDate();
+
+                // Retrieve the list of workouts for the clicked date from the workoutsMap
                 List<Workout> workoutForDate = workoutsMap.get(workoutDate);
 
+                // Check if workouts exist for the clicked date
                 if (workoutForDate != null && !workoutForDate.isEmpty()) {
+
+                    // Display information for each workout on the clicked date
                     for (Workout workout : workoutForDate) {
                         showAlert("Workout Information", workout.toString(), Alert.AlertType.INFORMATION);
                     }
@@ -207,13 +253,24 @@ public class Calendar implements Initializable {
      * @return Map with LocalDate as keys and ArrayList of Workout objects as values
      */
     private Map<LocalDate, ArrayList<Workout>> createWorkoutsMap(ArrayList<Workout> workoutList) {
+
+        // Initialize an empty HashMap to store workouts grouped by date
         Map<LocalDate, ArrayList<Workout>> workoutsMap = new HashMap<>();
 
+        // Iterate through each Workout object in the provided workoutList
         for (Workout workout : workoutList) {
+
+            // Retrieve the date of the current workout
             LocalDate workoutDate = workout.getDate();
+
+            // Check if the workoutsMap already contains the workout's date as a key
             if (!workoutsMap.containsKey(workoutDate)) {
+
+                // If the date is not present in the map, create a new list with the workout
                 workoutsMap.put(workoutDate, new ArrayList<>(List.of(workout)));
             } else {
+
+                // If the date is already a key, retrieve the existing list and add the workout to it
                 ArrayList<Workout> oldListByDate = workoutsMap.get(workoutDate);
                 oldListByDate.add(workout);
                 workoutsMap.put(workoutDate, oldListByDate);
